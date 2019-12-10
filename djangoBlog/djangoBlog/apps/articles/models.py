9e95d93a6c3ad015c11dde72ecb0c43b5df7c855
user0198 <jecka0198@gmail.com>
2019-12-10 22:45:57 +0200

    models Article, Comment created, db added

--- djangoBlog/djangoBlog/apps/articles/models.py
+++ djangoBlog/djangoBlog/apps/articles/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Article(models.Model):
+    article_title = models.CharField('article name', max_length=200)
+    article_text = models.TextField('article text')
+    pub_date = models.DateTimeField('date of publication')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days=7))
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField('name of author', max_length=50)
+    comment_text = models.CharField('comment text', max_length=200)
+
+    def __str__(self):
+        return self.author_name
--- djangoBlog/djangoBlog/apps/articles/views.py
+++ djangoBlog/djangoBlog/apps/articles/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello, world!')
+
+
+def test(request):
+    return HttpResponse('TEST PAGE')
